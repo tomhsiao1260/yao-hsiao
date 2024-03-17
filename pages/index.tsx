82aefdcf7c3281564a1b8e01ec97b7e070eaf136
Hsiao Yao <tomtogo1260@gmail.com>
2024-03-17 16:08:38 +0800

    a sketch for this month

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,9 @@ export default function Home() {
       <li className="flex px-10 justify-between gap-x-6 py-5">
         <Item title="Vesuvius Challenge 一週年的感想" date="2024.02.29" />
       </li>
+      <li className="flex px-10 justify-between gap-x-6 py-5">
+        <Item title="圖學相關 (標題未定)" date="2024.03.31" />
+      </li>
     </ul>
   );
 }
