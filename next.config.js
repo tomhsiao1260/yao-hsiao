d64c40cdc1e72299dc4190721fc62d69abc44bc7
Hsiao Yao <tomtogo1260@gmail.com>
2024-04-01 01:04:16 +0800

    fix formatting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const withMDX = require('@next/mdx')()
- 
+const withMDX = require("@next/mdx")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
-}
- 
-module.exports = withMDX(nextConfig)
+};
+
+module.exports = withMDX(nextConfig);
